a60ef67da285c924a7ae14fb22239580f5627abc
jakub014 <jakubn@hotmail.de>
2022-03-04 16:28:51 +0100

    Added preprocessing, replacement of numbers <= 10 by their actual words

---
+++ replaceNumbersWithLiterals.py
@@ -0,0 +1,31 @@
+import re
+
+
+numberMapping = [
+    (r'0', 'zero'),
+    (r'1', 'one'),
+    (r'2', 'two'),
+    (r'3', 'three'),
+    (r'4', 'four'),
+    (r'5', 'five'),
+    (r'6', 'six'),
+    (r'7', 'seven'),
+    (r'8', 'eight'),
+    (r'9', 'nine'),
+    (r'10', 'ten'),
+]
+
+outputFile = open("D:/Informationretrieval/anserini/collections/replacedNumbers.tsv", "a", encoding='utf-8')
+
+def readLinesfromFile(fileName):
+
+    with open(fileName, encoding='utf-8') as f:
+        for i,line in enumerate(f):
+            split = line.split("\t", 1)
+            resultLine = split[1]
+            for number, literal in numberMapping:
+                reg = r"\b"+ number +r"\b"
+                resultLine = re.sub(reg, literal, resultLine)
+            outputFile.write(split[0] + "\t" + resultLine)
+
+readLinesfromFile("D:/Informationretrieval/anserini/collections/msmarco-passage/collection.tsv")
